3da318c5f2036e3e9ab73deb6736b9d692593e8f
sviatoslav <svyatoslav.grinik@gmail.com>
2020-03-31 10:54:17 +0200

    added main class

---
+++ src/main/java/com/cursor/moviesrating/MoviesRatingApplication.java
@@ -0,0 +1,12 @@
+package com.cursor.moviesrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesRatingApplication.class, args);
+    }
+}
