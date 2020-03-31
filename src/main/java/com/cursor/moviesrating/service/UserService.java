ae1971455f98f99feb21b47a79623c311226d4de
sviatoslav <svyatoslav.grinik@gmail.com>
2020-03-31 11:01:44 +0200

    added service layer

---
+++ src/main/java/com/cursor/moviesrating/service/UserService.java
@@ -0,0 +1,13 @@
+package com.cursor.moviesrating.service;
+
+import com.cursor.moviesrating.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+    User save(User user);
+
+    User findUserByUsername(String username);
+
+}
---
+++ src/main/java/com/cursor/moviesrating/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.cursor.moviesrating.service.impl;
+
+import com.cursor.moviesrating.entity.User;
+import com.cursor.moviesrating.repository.UserRepository;
+import com.cursor.moviesrating.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return userRepository.findUserByUsername(username);
+    }
+
+}
